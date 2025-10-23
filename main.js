447261ba24800b30ec221f6f374648bbb3d1777a
giannivitale <giavitale78@gmail.com>
2025-10-23 16:07:27 +0200

    Update errors in js with teacher support

--- main.js
+++ main.js
@@ -10,13 +10,23 @@
  - Abbellire con CSS o Bootstrap
  - Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
  */
-for (let i = 0; i = 10; i++){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(response =>{
-    
-        const email = response.data;
-        //console.log(result);
+const emailList = document.getElementById("emailList")
 
+for (let i = 0; i < 10; i++){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => {
+    
+        const email = response.data.response;
+        //console.log(email);
+       // console.log(response.data.response);
+        
+        const li = document.createElement ('li')
+        //console.log(ul);
+        
+        li.textContent = email;
+        emailList.append(li);
+        //console.log(li);
+        
 }
 )}
 
